642893495fa1d18f704480e60b7282cac8e56e35
djstrong <djstrong/gmail>
2012-07-30 21:58:25 +0200

    fixed bugs, added running argument for language, readme, utility script for generating some languages

---
+++ generateLangs.sh
@@ -0,0 +1,10 @@
+python PLWiktionaryToDict.py plwiktionary-latest-pages-meta-current.xml > english_polish.txt
+python PLWiktionaryToDict.py plwiktionary-latest-pages-meta-current.xml "język niemiecki" > german_polish.txt
+python PLWiktionaryToDict.py plwiktionary-latest-pages-meta-current.xml "język francuski" > french_polish.txt
+python PLWiktionaryToDict.py plwiktionary-latest-pages-meta-current.xml "język hiszpański" > spanish_polish.txt
+python PLWiktionaryToDict.py plwiktionary-latest-pages-meta-current.xml "interlingua" > interlingua_polish.txt
+python PLWiktionaryToDict.py plwiktionary-latest-pages-meta-current.xml "esperanto" > esperanto_polish.txt
+python PLWiktionaryToDict.py plwiktionary-latest-pages-meta-current.xml "jidysz" > yiddish_polish.txt
+python PLWiktionaryToDict.py plwiktionary-latest-pages-meta-current.xml "język szwedzki" > swedish_polish.txt
+python PLWiktionaryToDict.py plwiktionary-latest-pages-meta-current.xml "język włoski" > italian_polish.txt
+python PLWiktionaryToDict.py plwiktionary-latest-pages-meta-current.xml "znak chiński" > han_polish.txt
